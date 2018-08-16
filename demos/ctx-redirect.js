ec112f180ba422b9c7673e4b97d7780f2e140671
张锋 <zhangfeng930101@qq.com>
2018-08-16 10:35:01 +0800

    test---fs

--- demos/ctx-redirect.js
+++ demos/ctx-redirect.js
@@ -1,15 +1,26 @@
 const Koa = require('koa')
 const app = new Koa()
+// 引入路由模块
 const route = require('koa-route')
+// 引入文件模块
+const fs = require('fs')
 
 const redirect = ctx => {
   ctx.response.redirect('/about')
   ctx.response.body = "<a href='/'>首页</a>";
 }
+
 const about = ctx => {
   ctx.response.type = 'html'
-  ctx.response.body = "<p>我是重定向的页面</p>"
+  ctx.response.body = "<p>我是重定向的页面</p><a href='/templateHtml'>点击回到首页</a>"
+}
+
+const templateHtml = ctx => {
+  ctx.response.type = 'html'
+  ctx.response.body = fs.createReadStream("./demos/test.html")
 }
 app.use(route.get('/redirect', redirect))
 app.use(route.get('/about', about))
+app.use(route.get("/templateHtml", templateHtml))
+
 app.listen(3030)
