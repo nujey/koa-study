cc77d0a93f9e7cdae0395021cd33d740e5920b23
张锋 <zhangfeng930101@qq.com>
2018-09-06 09:04:37 +0800

    s

--- demos/koa-ws.js
+++ demos/koa-ws.js
@@ -7,40 +7,16 @@ const websockify = require('koa-websocket')
 
 const app = websockify(new Koa())
 
-// app.ws.use(function(ctx, next) {
-//   console.log(ctx)
-//   return next(ctx)
-// })
-
 app.ws.use(route.all('/test', (ctx) => {
   ctx.websocket.send('我试着用node发送一条')
+  let num = 1
   ctx.websocket.on('message', function(message) {
-    ctx.websocket.send(message)
+    const param = {
+      id: num++,
+      msg: message
+    }
+    ctx.websocket.send(JSON.stringify(param))
   })
 }))
-// app.use(route.get('/test'), (ctx) => {
-//   ctx.response.type = 'html'
-//   ctx.response.body = "<a>'+message+'</a>"
-// })
+
 app.listen(9001)
-
-
-// Regular middleware
-// Note it's app.ws.use and not app.use
-// app.ws.use(function(ctx, next) {
-//   // return `next` to pass the context (ctx) on to the next ws middleware
-//   return next(ctx);
-// });
- 
-// Using routes
-// app.ws.use(route.all('/test/:id', function (ctx) {
-//   // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
-//   // the websocket is added to the context on `ctx.websocket`.
-//   ctx.websocket.send('Hello World');
-//   ctx.websocket.on('message', function(message) {
-//     // do something with the message from client
-//         console.log(message);
-//   });
-// }));
- 
-// app.listen(3000);
